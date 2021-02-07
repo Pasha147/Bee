e6d629fe1f1e859d20a58fc7181e9c891d0d2e7b
Pasha <pavel147223@gmail.com>
2021-02-07 09:48:26 +0200

    Use Classes

--- canvas.js
+++ canvas.js
@@ -9,12 +9,12 @@ $main.insertAdjacentHTML('beforeend',
 
 const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext("2d")
+///===================================
 
-//console.log(cWidth, cHeigh)
 
 let dist = 300
 let beecOn = false
-let speedBee = 0
+let beeAngr = 0
 
 let center = {
     x: 50,
@@ -25,22 +25,123 @@ let mousePosi = {
     y: 50
 }
 
+// Get the Bee angry (input asideRight)
 let input = document.getElementById('volume')
-
-input.addEventListener('change', function(){
-    speedBee=this.value/75
+input.addEventListener('change', function () {
+    beeAngr = this.value / 75
 }, false)
-  
-
-
-// input.addEventListener('change', function(){
-//   //  console.log(this.value)
-   
-// }, false)
 
 
 
 
+
+// Create the class AllGidota
+class AllGidota {
+    constructor() {
+
+        this.centr = { x: 0, y: 0 }
+        this.OnOf = false
+        this.visible = false
+
+    }
+
+    create() {
+
+    }
+
+    calcMove() {
+
+
+    }
+
+    drow(center) {
+
+    }
+}
+///===============================
+
+
+// Create the Bee class
+class Bee {
+    constructor(center, mousePosi) {
+        this.center = center
+        this.target = mousePosi
+    }
+
+    calcMove(beeAngr) {
+
+        let dx1 = 0
+        let dy1 = 0
+
+
+        if (this.target.x > this.center.x) dx1 = beeAngr
+        else {
+            if (this.target.x < this.center.x) dx1 = -beeAngr
+        }
+
+        if (this.target.y > this.center.y) dy1 = beeAngr
+        else {
+            if (this.target.y < this.center.y) dy1 = -beeAngr
+        }
+
+        let dx = (Math.floor(Math.random() * (3 + dx1)) - 1) * 6
+        let dy = (Math.floor(Math.random() * (3 + dy1)) - 1) * 6
+        if (dx >= 0 || this.center.x > 10) { this.center.x += dx }
+        if (dy >= 0 || this.center.y > 10) { this.center.y += dy }
+        return this.center
+    }
+
+    drow() {
+        let x = this.center.x
+        let y = this.center.y
+        ctx.lineWidth = 2
+        ctx.strokeStyle = "Black"
+        ctx.fillStyle = "Gold"
+        circle(x, y, 8, true)
+        circle(x, y, 8, false)
+        circle(x - 5, y - 11, 5, false)
+        circle(x + 5, y - 11, 5, false)
+        circle(x - 2, y - 1, 2, false)
+        circle(x + 2, y - 1, 2, false)
+
+    }
+
+}
+///========================================================
+
+let bee = new Bee(center, mousePosi)
+let bee1 = new Bee(center, mousePosi)
+bee1.center.x = rndab(20, cWidth-20)
+bee1.center.y = rndab(20, cHeigh-20)
+
+
+
+setInterval(function () {
+
+    if (beecOn) {
+        ctx.clearRect(0, 0, cWidth, cHeigh)
+
+        bee.drow(ctx)
+        center = bee.calcMove(beeAngr)
+
+        bee1.drow(ctx)
+        //bee1.calcMove(beeAngr)
+
+
+
+
+    }
+    else {
+        ctx.clearRect(0, 0, cWidth, cHeigh)
+    }
+
+    ball.draw()
+    ball.move()
+    ball.checkOut()
+    mousePosi.x = ball.x
+    mousePosi.y = ball.y
+
+}, 30)
 
 
 
@@ -55,17 +156,6 @@ const circle = function (x, y, radius, fillCircle) {
     }
 }
 
-const drawBee = function (x, y) {
-    ctx.lineWidth = 2
-    ctx.strokeStyle = "Black"
-    ctx.fillStyle = "Gold"
-    circle(x, y, 8, true)
-    circle(x, y, 8, false)
-    circle(x - 5, y - 11, 5, false)
-    circle(x + 5, y - 11, 5, false)
-    circle(x - 2, y - 1, 2, false)
-    circle(x + 2, y - 1, 2, false)
-}
 
 $('#canvas').click(cl = (event) => {
 
@@ -73,8 +163,8 @@ $('#canvas').click(cl = (event) => {
 
 
     if (beecOn) {
-        center.x = event.offsetX
-        center.y = event.offsetY
+        bee.center.x = event.offsetX
+        bee.center.y = event.offsetY
     }
 
 
@@ -83,6 +173,7 @@ $('#canvas').click(cl = (event) => {
 $('#canvas').mousemove(mmove = (event) => {
     mousePosi.x = event.offsetX
     mousePosi.y = event.offsetY
+
 })
 
 function rndab(a, b) {
@@ -147,41 +238,3 @@ Ball.prototype.checkOut = function () {
 
 let ball = new Ball()
 
-setInterval(function () {
-
-    if (beecOn) {
-        ctx.clearRect(0, 0, cWidth, cHeigh)
-        drawBee(center.x, center.y)
-
-        let dx1 = 0
-        let dy1 = 0
-        
-
-
-        if (mousePosi.x > center.x) dx1 = speedBee
-        else {
-            if (mousePosi.x < center.x) dx1 = -speedBee
-        }
-
-        if (mousePosi.y > center.y) dy1 = speedBee
-        else {
-            if (mousePosi.y < center.y) dy1 = -speedBee
-        }
-
-        let dx = (Math.floor(Math.random() * (3 + dx1)) - 1) * 6
-        let dy = (Math.floor(Math.random() * (3 + dy1)) - 1) * 6
-        if (dx >= 0 || center.x > 10) { center.x += dx }
-        if (dy >= 0 || center.y > 10) { center.y += dy }
-
-    }
-    else {
-        ctx.clearRect(0, 0, cWidth, cHeigh)
-    }
-
-    ball.draw()
-    ball.move()
-    ball.checkOut()
-    mousePosi.x = ball.x
-    mousePosi.y = ball.y
-
-}, 30)
